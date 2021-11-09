4d01f53454cca13fc93c9a3b8fd5909f256af8f5
leeyy2020 <liyueyang01@baidu.com>
2021-11-09 09:42:09 +0000

    change user

--- submit_k8s.sh
+++ submit_k8s.sh
@@ -19,11 +19,11 @@ if [[ ${nodes} -le 1 ]];then
 else
     start_file=./job_dist.sh
 fi
-#ak=28dd54f0908e548c81fd9302abf605f9
-#sk=ca3861e79a2d52098b19f52abf3a3b2a
+ak=4d2c6a826aad5de68c9a883006c9486c
+sk=7cf347fbedc0586ea905797d5c1c65b8
 
-ak=17635bcda14c5adfa6c2458d583457fb
-sk=8b08a400ecad5ede8ea196d5d47362d1
+# ak=17635bcda14c5adfa6c2458d583457fb
+# sk=8b08a400ecad5ede8ea196d5d47362d1
 
 
 /home/disk1/dqa/quyingqi/tools/software-install/paddlecloud-cli_2/paddlecloud job --ak ${ak} --sk ${sk} train \
